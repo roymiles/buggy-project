3a028aa1f3ddd65c9a85b5d757df2f6ea4139ede
Roy Miles <roy.miles12@gmail.com>
2016-11-17 12:04:28 +0000

    Commit problems

--- Buggy Project Arduino/Buggy Project Arduino/ImageProcessing.h
+++ Buggy Project Arduino/Buggy Project Arduino/ImageProcessing.h
@@ -35,28 +35,9 @@ public:
 	double getAngle(Mat image);
 
 private:
-<<<<<<< HEAD
 	float floorToCameraDistance;
 	const unsigned char red[3] = { 255,0,0 };
 	const unsigned char green[3] = { 0,255,0 };
 	const unsigned char blue[3] = { 0,0,255 };
-=======
-	Communications *c;
-
-	// Get image from the arduino module
-	CImg<unsigned char> getImage() {}
->>>>>>> 4512f29533787627fef620c274edc4d859907587
-
-	// Extracts a small region of the image. This step will make it easier to extract the edges of the two metal beams.
-	CImg<unsigned char> extractRegion(CImg<unsigned char>) {}
-	
-	// Convert the image to black and white for subsequent extraction of the edge coordinates using bwtraceboundary routine.
-	CImg<unsigned char> thresholdImage(CImg<unsigned char>) {}
-	
-	// Find intersection by moving from given position untill color changes. This will need to be performed twice
-	std::pair<int, int> traceToBoundary(int x, int y) {}
-	
-	// Find the angle of the line intersecting the two points
-	double getAngle(std::pair<int, int>, std::pair<int, int>) {}
 };
 
