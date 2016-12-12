12c70acb7d1be4479c0c9364143a7f5bece888d1
iyop45 <roy.miles12@gmail.com>
2016-12-12 12:46:58 +0000

    Ultrasonic collision semi-working

--- IDE Code/Buggy_Project/UltraSonicSensor.h
+++ IDE Code/Buggy_Project/UltraSonicSensor.h
@@ -5,10 +5,19 @@
 
     @author Roy Miles
     @version 1.0 11/12/2016
+
+    HC-SR04 Ping distance sensor:
+    VCC to arduino 5v 
+    GND to arduino GND
+    Echo to Arduino pin 7 
+    Trig to Arduino pin 8
 */
 
 #ifndef UltraSonicSensor_h
 #define UltraSonicSensor_h
+
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
 
 #include "Arduino.h"
 #include "CollisionSensor.h" // This object is of type "CollisionSensor"
@@ -19,8 +28,12 @@ class UltraSonicSensor: public CollisionSensor
 public:
 	UltraSonicSensor();
 	~UltraSonicSensor();
+  
+  int maximumRange = 200; // Maximum range needed
+  int minimumRange = 0; // Minimum range needed
+  long duration, distance; // Duration used to calculate distance
 
-	// Return in mm the distance to an object
+	// Return in cm the distance to an object
 	double getDistanceToNearestObject();
 
   bool isCollision();
