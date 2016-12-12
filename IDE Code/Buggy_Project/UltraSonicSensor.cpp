0b626ebda457e7e028834c638e9c961ef1f2f54d
iyop45 <roy.miles12@gmail.com>
2016-12-12 12:46:30 +0000

    Ultrasonic sensors working (semi)

--- IDE Code/Buggy_Project/UltraSonicSensor.cpp
+++ IDE Code/Buggy_Project/UltraSonicSensor.cpp
@@ -5,6 +5,12 @@
 
     @author Roy Miles
     @version 1.0 11/12/2016
+
+    HC-SR04 Ping distance sensor:
+    VCC to arduino 5v 
+    GND to arduino GND
+    Echo to Arduino pin 7 
+    Trig to Arduino pin 8
 */
 
 
@@ -12,6 +18,10 @@
 
 UltraSonicSensor::UltraSonicSensor()
 {
+  Serial.write("Ultrasonic sensor module...\n");
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 
@@ -20,10 +30,34 @@ UltraSonicSensor::~UltraSonicSensor()
 }
 
 double UltraSonicSensor::getDistanceToNearestObject(){
-	// EMPTY
+  /* The following trigPin/echoPin cycle is used to determine the
+  distance of the nearest object by bouncing soundwaves off of it. */ 
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2); 
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = duration/58.2;
+  
+  if (distance >= maximumRange || distance <= minimumRange){
+    /* Send a negative number to computer and Turn LED ON 
+    to indicate "out of range" */
+    Serial.println("-1");
+  }
+  else {
+    /* Send the distance to the computer using Serial protocol, and
+    turn LED OFF to indicate successful reading. */
+    Serial.println(distance);
+  }
+
+  return distance;
 }
 
 bool UltraSonicSensor::isCollision(){
   // EMPTY
 }
-
